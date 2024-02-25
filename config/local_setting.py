08f35bf1594cf44d2b78ce4321ebb8fa65cfa460
Mostafa Darabi <mdarabi1369@gmail.com>
2024-02-25 22:58:06 +0330

    send to github

---
+++ config/local_setting.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-qlbn5%wx%d-0(3robi78=_q3locp)#!3ebe1__$4jf07!6o*=p'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
