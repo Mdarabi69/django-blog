83b98c0845706132378c12013d571c1f6a15927e
مصطفی دارابی <m.darabi@localserver.tsk>
2024-02-26 09:40:55 +0330

    add-account

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views import generic
 
-# Create your views here.
+
+
+    
+
+
+
+
+
