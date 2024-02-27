98952b1a4cd09b57d56e22d340b1ba6ecf699931
Mostafa Darabi <mdarabi1369@gmail.com>
2024-02-27 22:10:03 +0330

    complate Account

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
     success_url = reverse_lazy('login')
-    model = User
-    form_class = UserCreationForm
+    model = CustomUser
+    form_class = CustomUserCreationForm
     template_name ='registration/signup.html'
+    
     
 
 
--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -120,5 +123,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_REDIRECT_URL ='home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
